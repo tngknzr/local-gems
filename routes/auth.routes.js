75f98253d5c8acb2eb7db2d617260684ac9ae606
tngknzr <agustinparedes95@gmail.com>
2023-07-31 15:15:52 +0200

    authentication2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = new Router();
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
-
+const User = require('../models/User.model');
 router.get('/signup', (req, res) => res.render('auth/signup'));
 
 router.post('/signup', (req, res, next) => {
@@ -14,8 +14,19 @@ router.post('/signup', (req, res, next) => {
       .then(salt => bcrypt.hash(password, salt))
       .then(hashedPassword => {
         console.log(`Password hash: ${hashedPassword}`);
+        return User.create({
+           
+            username,
+            email,
+            
+            passwordHash: hashedPassword
+          });
+      })
+      .then(userFromDB => {
+        res.redirect('/user-profile');
       })
       .catch(error => next(error));
   });
+  router.get('/user-profile', (req, res) => res.render('user/user-profile'));
  
 module.exports = router;
