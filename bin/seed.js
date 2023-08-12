5972f7312b9b5dca71e6563a6fde78212e27d582
Biljana Kotevska <biljanakotev@gmail.com>
2023-08-12 13:44:47 +0200

    added require dotenv script

--- bin/seed.js
+++ bin/seed.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Gem = require('../models/Gem.model');
+require('dotenv').config();
 
 // const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/local';
 
