9b713598109a0aa1cbe55296a1502c40da120165
tngknzr <agustinparedes95@gmail.com>
2023-08-12 14:59:18 +0200

    styling home,sing,log

--- config/session.config.js
+++ config/session.config.js
@@ -22,7 +22,7 @@ module.exports = app => {
         maxAge: 3540000 // 60 * 1000 ms === 1 min
       },
       store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/basic-auth'
+        mongoUrl: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/local'
  
        
       })
